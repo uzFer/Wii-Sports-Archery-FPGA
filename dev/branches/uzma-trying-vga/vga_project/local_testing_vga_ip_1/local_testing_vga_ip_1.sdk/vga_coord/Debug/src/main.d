1e426731023964c80e763f4147b904c218ed9c8e
unknown <celine.kwan2017@gmail.com>
2026-03-15 12:46:39 -0400

    added microblaze code for testing state and coordinate change, need to fix x and y coordinates not updating crosshair position

---
+++ dev/branches/uzma-trying-vga/vga_project/local_testing_vga_ip_1/local_testing_vga_ip_1.sdk/vga_coord/Debug/src/main.d
@@ -0,0 +1,43 @@
+src/main.o src/main.o: ../src/main.c \
+ ../../vga_coord_bsp/microblaze_0/include/xparameters.h \
+ ../../vga_coord_bsp/microblaze_0/include/xil_cache.h \
+ ../../vga_coord_bsp/microblaze_0/include/mb_interface.h \
+ ../../vga_coord_bsp/microblaze_0/include/xil_types.h \
+ ../../vga_coord_bsp/microblaze_0/include/xil_assert.h \
+ ../../vga_coord_bsp/microblaze_0/include/xil_exception.h \
+ ../../vga_coord_bsp/microblaze_0/include/xparameters.h \
+ ../../vga_coord_bsp/microblaze_0/include/xintc.h \
+ ../../vga_coord_bsp/microblaze_0/include/xstatus.h \
+ ../../vga_coord_bsp/microblaze_0/include/xintc_l.h \
+ ../../vga_coord_bsp/microblaze_0/include/xil_io.h \
+ ../../vga_coord_bsp/microblaze_0/include/xil_printf.h \
+ ../../vga_coord_bsp/microblaze_0/include/bspconfig.h \
+ ../../vga_coord_bsp/microblaze_0/include/framewriter.h
+
+../../vga_coord_bsp/microblaze_0/include/xparameters.h:
+
+../../vga_coord_bsp/microblaze_0/include/xil_cache.h:
+
+../../vga_coord_bsp/microblaze_0/include/mb_interface.h:
+
+../../vga_coord_bsp/microblaze_0/include/xil_types.h:
+
+../../vga_coord_bsp/microblaze_0/include/xil_assert.h:
+
+../../vga_coord_bsp/microblaze_0/include/xil_exception.h:
+
+../../vga_coord_bsp/microblaze_0/include/xparameters.h:
+
+../../vga_coord_bsp/microblaze_0/include/xintc.h:
+
+../../vga_coord_bsp/microblaze_0/include/xstatus.h:
+
+../../vga_coord_bsp/microblaze_0/include/xintc_l.h:
+
+../../vga_coord_bsp/microblaze_0/include/xil_io.h:
+
+../../vga_coord_bsp/microblaze_0/include/xil_printf.h:
+
+../../vga_coord_bsp/microblaze_0/include/bspconfig.h:
+
+../../vga_coord_bsp/microblaze_0/include/framewriter.h:
